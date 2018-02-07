19eea7f27cf0c8f1b0ca2e68cea4e7cc308ed574
Jit <suran.basu@gmail.com>
2018-02-07 20:52:08 +0530

    deleted manifest.json

--- hospital_best_practices.php
+++ hospital_best_practices.php
@@ -1,18 +1,4 @@
-<?php
 
-
-?>
-
-
-<head>
-<title>Best Practices</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="./css/village.css">
-
-
-  </head>
-  <body>
       <div class="col-12 heading" >
         <h1>VICTech</h1>
         <h4>Village Information and Communication Technology</h4>
@@ -25,5 +11,3 @@
     </div>
     <div class="col-6">
     </div>
-  </body>
-</html>
--- hospital_common_ailments.php
+++ hospital_common_ailments.php
@@ -4,25 +4,29 @@
         <h4></h4>
         <!--What is RICTech -->
 
-    </div>
-    <div class="col-1"></div><div class="col-1 button t_center" onclick="goBack();"><h4><b>Back</b></h4></div><div class="col-3"></div><div class="col-2" ></div><div class="col-3"></div><div class="col-1 button t_center" onclick="loadPage('index.php')">
-      <h4><b>Home</b></h4></div><div class="col-1"></div>
-
-<div class="col-10 t_center t_white">
-      <br />
-
+      </div>
+      <div class="col-1"></div>
+      <div class="col-1 button t_center" onclick="goBack();"><h4><b>Back</b></h4></div>
+      <div class="col-3"></div>
+      <div class="col-2" ></div>
+      <div class="col-3"></div>
+      <div class="col-1 button t_center" onclick="loadPage('index.php')">
+      <h4><b>Home</b></h4>
+      </div>
+      <div class="col-1"></div>
+      <div class="col-10 t_center t_white">
+        <br />
         Search By Symptoms: <input type="text" name="symptom" id="symptom" onkeyup="loadsickness();">
-<table class="table-fill">
-<thead>
-<tr>
-<th class="text-left">Sickness</th>
-<th class="text-left">Symptoms</th>
-<th class="text-left">Best Practices</th>
-</tr>
-</thead>
-<tbody class="table-hover" id="sickness_info">
+        <table class="table-fill">
+          <thead>
+            <tr>
+              <th class="text-left">Sickness</th>
+              <th class="text-left">Symptoms</th>
+              <th class="text-left">Best Practices</th>
+            </tr>
+          </thead>
+          <tbody class="table-hover" id="sickness_info">
 
-
-</tbody>
-</table>
-</div>
+          </tbody>
+        </table>
+      </div>
