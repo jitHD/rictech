dc10989e52e12ae54109354025f62abdbadb70e2
jitHD <suran.basu@gmail.com>
2018-05-23 18:12:48 +0530

    removed duplicate

--- upload_files.php
+++ upload_files.php
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-
-
-
-<html>
-<head>
-</head>
-<body>
-<form action="upload_files_action.php" method="post" enctype="multipart/form-data">
-<table>
-  <tr><td>Photo 1:</td><td><input type="file" name="file_1" required></td></tr>
-  <tr><td>Photo 2:</td><td><input type="file" name="file_2" required></td></tr>
-  <tr><td>Photo 3:</td><td><input type="file" name="file_3" required></td></tr>
-  <tr><td>Photo 4:</td><td><input type="file" name="file_4" required></td></tr>
-  <tr><td colspan="2" align="center"><input type="submit" value="Submit">&nbsp;<input type="reset" value="Reset"></td></tr>
-</table>
-</form>
-</body>
-</html>
-=======
-
 
 
 <html>
@@ -36,4 +15,3 @@
 </form>
 </body>
 </html>
->>>>>>> 629f4003704f19be092a4faed21ab956e331ca8d
