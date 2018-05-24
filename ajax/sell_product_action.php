07c54deaa2c02b635b7baea7ea301877b8e7264a
jitHD <suran.basu@gmail.com>
2018-05-24 13:14:36 +0530

    moved sell_product_action

---
+++ ajax/sell_product_action.php
@@ -0,0 +1,38 @@
+
+<?php
+
+session_start();
+$seller=$_SESSION['phone'];
+echo $seller;
+$type=$_SESSION['product_type'];
+echo "<br>".$type;
+$_SESSION['product_name']=$_POST['product'];
+
+echo "<br>";
+
+$id=$seller.date("YmdHis");
+echo $id;
+$product_name=$_POST['product'];
+echo "<br>".$product_name;
+$quantity=$_POST['quantity'];
+echo "<br>".$quantity;
+$unit=$_POST['unit'];
+echo "<br>".$unit;
+$price=$_POST['price'];
+echo "<br>".$price;
+
+require_once '../connect.php';
+//$conn = mysqli_connect("localhost","root","root","buy_sell");
+
+if(mysqli_query($conn,"INSERT INTO `$type` (`id`, `seller`, `product_name`, `photo_1`, `photo_2`, `photo_3`, `photo_4`, `quantity`, `unit`, `price`, `number_of_requests`, `add_date`) VALUES ('$id', '$seller', '$product_name', '', '', '', '', '$quantity', '$unit', '$price', '0', CURRENT_TIMESTAMP)"))
+{
+  $_SESSION['product_id']=$id;
+  //header("location:upload_files.php");
+
+}
+else {
+  $msg="Something went wrong";
+  echo $msg;
+  //header("location:sell_product.php?msg=".$msg);
+}
+?>
