2d71bf1a57c42fadc222cc53416db89799bc153f
jitHD <suran.basu@gmail.com>
2018-05-24 11:36:15 +0530

    something

---
+++ disaster_recovery.php
@@ -0,0 +1,47 @@
+<head>
+<title>Ambulance Request</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="./css/village.css">
+
+  </head>
+  <body>
+      <div class="col-12 t_center t_white heading" >
+        <h1>RICTech: Disaster Response</h1>
+
+
+      </div>
+      <div class="col-1 empty"></div><div class="col-1 button t_center" onclick="goBack();"><h4><b>Back</b></h4></div><div class="col-3 empty"></div><div class="col-2 empty" ></div><div class="col-3 empty"></div><div class="col-1 button t_center" onclick="loadPage('index.php')">
+      <h4><b>Home</b></h4></div><div class="col-1 empty"></div>
+      <div class="col-12"><h2 align="center" style="color:red;">Udalguri Emergency Response: (03711)224433/224281</h2></div>
+      <div class="col-6 t_white t_center" >
+
+        <table align="center">
+          <tr><th>Disaster Dept.</th><th>Phone Number</th></tr>
+          <tr><td>State Control Room, Dispur</td><td class="t_center">1070<br>1079<br>1077<br>0361-2237219<br>0361-2237460</td></tr>
+
+
+
+          <tr><th>Fire Brigade</th><th>Phone Number</th></tr>
+          <tr><td>Panbazar Fire Brigade</td><td class="t_center">0361-2540222</td></tr>
+          <tr><td>Dispur Fire Brigade</td><td class="t_center">0361-2260221</td></tr>
+        </table>
+
+      </div>
+      <div class="col-6 t_white t_center">
+        <table align="center">
+          <tr><th>Ambulance.</th><th>Phone Number</th></tr>
+          <tr><td>G. L. P. Publicationr</td><td class="t_center">0361-2544357</td></tr>
+          <tr><td>Marowary Yuva Manch</td><td class="t_center">0361-2542074 / 2547251</td></tr>
+          <tr><td>Lion’s Club</td><td class="t_center">0361-2545220</td></tr>
+
+
+          <tr><th>Dead Body Carrying Van</th><th>Phone Number</th></tr>
+          <tr><td>G. L. P. Publication</td><td class="t_center">0361-2737373</td></tr>
+          <tr><td>Marowary Yuva Manch</td><td class="t_center">0361-2547251</td></tr>
+        </table>
+
+      </div>
+
+  </body>
+</html>
