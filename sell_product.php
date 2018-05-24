825166a9f740b7804a9253905871dd842928b4f0
jitHD <suran.basu@gmail.com>
2018-05-24 13:28:23 +0530

    sell_product_redirect testing

--- sell_product.php
+++ sell_product.php
@@ -3,29 +3,41 @@
 if(isset($_GET['type']))
 {
   session_start();
-  echo $_SESSION['phone'];
+  //echo $_SESSION['phone'];
   $_SESSION['product_type']=$_GET['type'];
-  echo $_SESSION['product_type'];
+  //echo $_SESSION['product_type'];
 ?>
 
 <html>
 <head>
 </head>
 <body>
-  <form action="sell_product_action.php" method="post">
-    <table>
-      <tr><td>Product Name:</td><td><input type="text" name="product" required></td></tr>
 
-      <tr><td>Quantity:</td><td><input type="number" name="quantity" required>
-        <select name="unit" required>
+  <div class="col-1 empty"></div>
+  <div class="col-1 button t_center" onclick="goBack();"><h4><b>Back</b></h4></div>
+  <div class="col-3 empty"></div>
+  <div class="col-2 empty" ></div>
+  <div class="col-3 empty"></div>
+  <div class="col-1 button t_center" onclick="loadPage('index.php')"> <h4><b>Home</b></h4></div>
+  <div class="col-1 empty"></div>
+<div class="col-12">
+  <!-- <form action="sell_product_action.php" method="post"> -->
+  <form id="product_input" method="post">
+    <table align="center">
+      <tr><td>Product Name:</td><td><input id="pruduct_name" type="text" name="product" required></td></tr>
+
+      <tr><td>Quantity:</td><td><input id="quantity" type="number" name="quantity" required>
+        <select id="unit" name="unit" required>
           <option value="g">Grams</option>
           <option value="Kg">Kilograms</option>
           <option value="P">Piece</option>
         </select>
         </td></tr>
-      <tr><td>Price per quantity: </td><td><input type="float" name="price"></td></tr>
-      <tr><td colspan="2" align="center"><input type="submit" value="Submit">&nbsp;<input type="reset" value="Clear"></td></tr>
+      <tr><td>Price per quantity: </td><td><input id="price" type="float" name="price"></td></tr>
+      <tr><td colspan="2" align="center" onclick="return req_form_action();"><input type="submit" value="Submit">&nbsp;<input type="reset" value="Clear">
+        <p style="color:Red;font-size:30px;" id="add_product"></p></td></tr>
   </form>
+</div>
 </body>
 
 </html>
