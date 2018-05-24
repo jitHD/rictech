a42fd213a01f581a4ca4e0607e49227c4b1fd936
jitHD <suran.basu@gmail.com>
2018-05-24 21:22:06 +0530

    small matter

--- upload_files.php
+++ upload_files.php
@@ -9,12 +9,12 @@
 <!-- <form action="upload_files_action.php" method="post" enctype="multipart/form-data"> -->
 <form id="uploadimage" enctype="multipart/form">
 <table align="center">
-  <tr><td>Photo 1:</td><td><input type="file" id="file" name="file_1" required></td></tr>
+  <tr><td>Photo 1:</td><td><input type="file" id="file_1" name="file_1" required></td></tr>
   <tr><td>Photo 2:</td><td><input type="file" id="file_2" name="file_2" required></td></tr>
   <tr><td>Photo 3:</td><td><input type="file" id="file_3" name="file_3" required></td></tr>
   <tr><td>Photo 4:</td><td><input type="file" id="file_4" name="file_4" required></td></tr>
 
-  <tr><td colspan="2" align="center"><input type="submit" onclick="return uploadFile();" value="Submit">&nbsp;<input type="reset" value="Reset"><p id="loading"></p><p id="message"></p></td></tr>
+  <tr><td colspan="2" align="center"><input type="submit" value="Submit">&nbsp;<input type="reset" value="Reset"><p id="loading"></p><p id="message"></p></td></tr>
 </table>
 </form>
 </div>
