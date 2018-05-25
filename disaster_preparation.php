65a7842e408ff6d26e2dc54aba753d55853936f1
jitHD <suran.basu@gmail.com>
2018-05-25 21:38:44 +0530

    disaster management

---
+++ disaster_preparation.php
@@ -0,0 +1,66 @@
+<head>
+<title>Ambulance Request</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="./css/village.css">
+
+  </head>
+  <body>
+      <div class="col-12 t_center t_white heading" >
+        <h1>RICTech: Disaster Preparation</h1>
+
+
+      </div>
+      <div class="col-1 empty"></div><div class="col-1 button t_center" onclick="goBack();"><h4><b>Back</b></h4></div><div class="col-3 empty"></div><div class="col-2 empty" ></div><div class="col-3 empty"></div><div class="col-1 button t_center" onclick="loadPage('index.php')">
+      <h4><b>Home</b></h4></div><div class="col-1 empty"></div>
+      <div class="col-12"><h2 align="center"><a href="http://onlineasdma.assam.gov.in/" style="text-decoration:none; color:red;">Assam State Disaster Management Authority GIS</a></h2></div>
+      <div class="col-6 t_white t_center" >
+
+        <table align="center">
+          <tr><th colspan="5">Drinking Water Source </th></tr>
+          <tr><th>Area</th><th>Section Name</th><th>Area Officer i/c</th><th>Contact No</th><th>Zonal Officer</th></tr>
+          <tr><td>Tangla & Panery</td><td>Khalngduar</td><td>Mr.Jisua Daimari</td><td>9854645212</td><td>A.E.E.(PHE)<br> Tangla-9678240674</td></tr>
+          <tr><td>Dimakuchi</td><td>Nonai Sarfang</td><td>Mr. Nagandra Bharati</td><td>9401142861</td><td>A.E.E.(PHE)<br> Tangla-9678240674</td></tr>
+          <tr><td>Bhergaon</td><td>Bhergaon</td><td>Mr.Mridul Baruah JE</td><td>9864884181</td><td>A.E.E.(PHE)<br> Tangla-9678240674</td></tr>
+          <tr><td>Kalaigaon</td><td>Mudaibari (Part)</td><td>Mr. Sanjay Das. JE</td><td>9864947365</td><td>A.E.E.(PHE)<br> Kalaigaon 9435184534</td></tr>
+          <tr><td>Kalaigaon</td><td>Mudaibari (Part)</td><td>Mr. Khagan Boro. JE</td><td>9854158878</td><td>A.E.E.(PHE)<br> Kalaigaon 9435184534</td></tr>
+          <tr><td>Kalaigaon</td><td>Harisinga (Part)</td><td>Mr. Jyotish Hazarika.AE</td><td>9854714198</td><td>A.E.E.(PHE)<br> Kalaigaon 9435184534</td></tr>
+          <tr><td>Khoirabari</td><td>Khoirabari</td><td>Mr. Dimbeswar sarmah, JE.</td><td>8011408360</td><td>A.E.E.(PHE)<br> Kalaigaon 9435184534</td></tr>
+          <tr><td>Rowta</td><td>Rowta</td><td>Mr. Jogen Choudhury.JE.</td><td>9435636971</td><td>A.E.E.(PHE)<br> Udalguri 9435060271</td></tr>
+          <tr><td>Harisinga</td><td>Harisinga (Part)</td><td>Mr. Lohit Baruah,JE.</td><td>-------</td><td>A.E.E.(PHE)<br> Udalguri 9435060271</td></tr>
+          <tr><td>Udalguri</td><td>Dhansiri</td><td>Mr.Tankaeswar Bania,JE.</td><td>9859525559</td><td>A.E.E.(PHE)<br> Udalguri 9435060271</td></tr>
+          <tr><td>Mazbat</td><td>Pasnoi Serfang</td><td>Mr.Paresh Dutta Bhuyan,JE.</td><td>9864179645</td><td>A.E.E.(PHE)<br> Udalguri 9435060271</td></tr>
+          <tr><td>Bhairabkunda</td><td>Bhairabkunda</td><td>Mr.Paresh Dutta Bhuyan,JE.</td><td>---------</td><td>A.E.E.(PHE)<br> Udalguri 9435060271</td></tr>
+
+
+        </table>
+
+      </div>
+      <div class="col-6 t_white t_center">
+        <table align="center">
+          <tr><th colspan="3">Food and Civil Supply (Udalguri)</th></tr>
+          <tr><th colspan="3">F.C.I Tangla: 9435007148</th></tr>
+          <tr><th>Name</th><th>Designation</th><th>Contact No</th></tr>
+          <tr><td>Sri, Bhudhar Das</td><td>Asst. Director of Food and Civil Supplies & <br>consumer Affairs, Udalguri.</td><td>9435188903</td></tr>
+          <tr><td>Sri Bhupen Bayan</td><td>Inspector, F.C.S & C.A, Udalguri</td><td>9854031016</td></tr>
+          <tr><td>Sri kabindra Das</td><td>Supperintend, F.C.S & C.A, Bhergaon</td><td>9435022630</td></tr>
+          <tr><td>Sri Sushanta paul</td><td>Inspector F.C.S & C.A, Bhergaon</td><td>9435193395</td></tr>
+          <tr><td>Sri Hargobinda Ojha</td><td>Inspector F.C.S & C.A, Bhergaon</td><td>9706060460</td></tr>
+          <tr><td>Sri Ranjit Das</td><td>L.D.A F.C.S & C.A, Udalguri</td><td>9864827360</td></tr>
+          </table>
+          <br><br>
+          <table align="center">
+            <tr><th colspan="3">Udalguri Electricity Divisions</th></tr>
+
+            <tr><th>Area Name</th><th>Contact Person</th><th>Office</th><th>Mobile</th></tr>
+            <tr><td>Deputy General Manger</td><td>Sri.T.N Sharma</td><td>03711-22125</td><td>9435028265</td></tr>
+            <tr><td>Udalguri Division</td><td>Mr.B.Bora, AGM</td><td>03711-224402</td><td>9508432326</td></tr>
+            <tr><td>Tangla division</td><td>Mr.A.baruah, S.DE</td><td>03711-2255418</td><td>9435192496</td></tr>
+            <tr><td>Mazbat Sub-Division</td><td>Mr.Ankur Das,SDE</td><td>--------</td><td>9864625496</td></tr>
+            <tr><td>Udalguri Sub-division</td><td>Mr.Durga Dutta,SDE</td><td>03711-224440</td><td>9508159155</td></tr>
+            </table>
+
+      </div>
+<div class="col-12">Disclaimer: The above information has been uploaded with reference to the assam state disaster management datasheet for udalguri district from http://sdmassam.nic.in/pdf/dmp/Udalguri.pdf</div>
+  </body>
+</html>
--- disaster_response.php
+++ disaster_response.php
@@ -17,11 +17,8 @@
       <div class="col-6 t_white t_center" >
 
         <table align="center">
-          <tr><th>Disaster Dept.</th><th>Phone Number</th></tr>
+          <tr><th>Department</th><th>Phone Number</th></tr>
           <tr><td>State Control Room, Dispur</td><td class="t_center">1070<br>1079<br>1077<br>0361-2237219<br>0361-2237460</td></tr>
-
-
-
           <tr><td>Udalguri Fire Station</td><td>98597-91345, 03711-224959</td></tr>
           <tr><td>Tangla Fire Station</td><td>96132-55238, 03711- 255495
           <tr><td>Panbazar Fire Brigade</td><td class="t_center">0361-2540222</td></tr>
@@ -32,18 +29,17 @@
       <div class="col-6 t_white t_center">
 
         <table align="center">
+          <tr><td>Udalguri District Control Room</td><td>03711-225502/108</td></tr>
           <tr><th>Ambulance</th><th>Phone Number</th></tr>
           <tr><td>G. L. P. Publicationr</td><td class="t_center">0361-2544357</td></tr>
           <tr><td>Marowary Yuva Manch</td><td class="t_center">0361-2542074 / 2547251</td></tr>
           <tr><td>Lion’s Club</td><td class="t_center">0361-2545220</td></tr>
-
-
           <tr><th>Dead Body Carrying Van</th><th>Phone Number</th></tr>
           <tr><td>G. L. P. Publication</td><td class="t_center">0361-2737373</td></tr>
           <tr><td>Marowary Yuva Manch</td><td class="t_center">0361-2547251</td></tr>
         </table>
 
       </div>
-
+      <div class="col-12">Disclaimer: The above given information has been uploaded with reference to information obtained from the Assam state disaster management website and datasheet for Udalguri district</div>
   </body>
 </html>
